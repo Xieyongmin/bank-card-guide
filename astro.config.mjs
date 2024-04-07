b1ffb22df85f7b4a4065a131961d1af51663c60b
Joe <xym-fx@163.com>
2024-04-08 07:58:44 +0800

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
-  }), compress()],
+  }), compress(), partytown({
+    forward: ['dataLayer.push'],
+})],
 })
