f2345af87a611ad0293a8b07c23ddbbd6c495e1d
Joe <xym-fx@163.com>
2024-04-09 07:00:11 +0800

    增加sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
 import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
+  site:'https://bankcardguide.midware.dev',
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
   }), compress(), partytown({
     forward: ['dataLayer.push'],
-})],
+}), sitemap()],
 })
